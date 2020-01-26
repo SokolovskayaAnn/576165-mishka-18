3b2db9e2974bf4432702b5fd3f5c9520df631317
SokolovskayaAnn <sokolovskaya.ann@mail.ru>
2020-01-26 03:39:08 +0300

    Автоматизирует сборк проекта

--- source/js/modal-show.js
+++ source/js/modal-show.js
@@ -1,12 +1,25 @@
 var link = document.querySelector('.hit__order-button');
+var basketLink = document.querySelectorAll('.list-item__basket-link');
 var popup = document.querySelector('.modal');
 var overlay = document.querySelector('.overlay');
 
-link.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  popup.classList.add('modal--show');
-  overlay.classList.add('overlay--show');
-});
+if (link) {
+  link.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    popup.classList.add('modal--show');
+    overlay.classList.add('overlay--show');
+  });  
+}
+
+if (basketLink) {
+  for (var i = 0; i < basketLink.length; i++) {
+    basketLink[i].addEventListener('click', function(evt) {
+      evt.preventDefault();
+      popup.classList.add('modal--show');
+      overlay.classList.add('overlay--show');
+    });
+  }
+}
 
 window.addEventListener('keydown', function(evt) {
   if (evt.keyCode === 27) {
